d7a985888db2f7a4973bcc925af3a2b88420fd43
rr37 <ay870527@gmail.com>
2023-04-19 14:49:08 +0800

    feat: add form to index.hbs

--- app.js
+++ app.js
@@ -25,7 +25,15 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const url = req.body
+  console.log(url)
   res.render('index')
 })
 
